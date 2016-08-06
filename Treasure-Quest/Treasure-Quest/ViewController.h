affc5c07d9074f0112f4e371be71a85662bd348e
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-08-05 20:25:30 -0700

    initial commit for location services

--- Treasure-Quest/Treasure-Quest/ViewController.h
+++ Treasure-Quest/Treasure-Quest/ViewController.h
@@ -7,8 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+@import MapKit;
+@import UIKit;
+@import CoreLocation;
+
+@protocol LocationManagerDelegate <NSObject>
+
+-(void)locationControllerDidUpdateLocation:(CLLocation *)location;
+
+@end
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLLocation *location;
+
+@property (weak, nonatomic) id delegate;
+
++(ViewController *)sharedController;
 
 
 @end
